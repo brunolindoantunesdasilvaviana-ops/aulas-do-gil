bcc12653a9f4fb5a028403f0377cc11a088adcec
brunolindoantunesdasilvaviana-ops <brunolindoantunesdasilvaviana@gmail.com>
2026-04-26 17:09:35 -0300

    Add files via upload

--- aulas-do-gil/atv9.c
+++ aulas-do-gil/atv9.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 
 int main () {
-    int alt, aux = 1;
+    int alt, aux = 1, esp, aux2;
 
     printf("Digite a altura:");
     scanf("%i", &alt);
 
+    aux2 = alt;
 
     for(int i = 1; i <= alt; i += 2) {
-        for(; aux <= i ; aux ++) {
-            if(aux == i) {
-                printf("\n");
-            }
-            if(aux != i) {
-                printf("O");
-            }
+        aux2 -= 2;
+        esp = 1;
+        for(; esp <= aux2; esp +=2) {
+            printf(" ");
         }
-        aux = 0;
+        for(int j = 0; j < i; j++) {
+            printf("O");
+        }
+        printf("\n");
     }
-
-
 
     return 0;
 }
